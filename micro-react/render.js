a38ddf55cdac52cecca73a0411136f8f4bb5f427
Dr-ZHUIM <372919916@qq.com>
2022-08-22 13:19:12 +0800

    concurrent render

--- micro-react/render.js
+++ micro-react/render.js
@@ -6,12 +6,29 @@ function render(element, container) {
     //设置属性
     Object.keys(element.props).filter(key => key !== 'children')
     .map(key=>(dom[key] = element.props[key]))
-    //递归渲染子元素
-    element.props.children.map(child=>{
-        render(child,dom);
-    });
     //追加至父元素
     container.append(dom);
+}
+
+let nextUnitOfWork = null;
+
+function workLoop(){
+    //应该退出
+    let shouldYield = false;
+    //有工作且有时间运行时
+    while(nextUnitOfWork && !shouldYield){
+        nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+        //判断时间是否足够
+        shouldYield = deadLine.timeRemaining() < 1;
+    };
+    //没有时间就再次请求
+    requestIdleCallback(workLoop);
+};
+
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(work){
+    
 }
 
 export default render
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # 手撕React
 
+## steps
+
 ### Step1: `createElement` function
 ### Step2: `render` function
 ### Step3: Concurrent Mode
@@ -8,3 +10,21 @@
 ### Step6: Reconcilication
 ### Step7: Function Components
 ### Step8: React-Hook
+
+---
+
+## point of knowledge
+
+### 1. Fiber 架构是什么？
+
+**定义：**  
+Fiber是一种架构，用于解决 react 15 时的一个问题 ： 一旦react重新渲染，便不能中断，如果这次渲染的层级很深，这将造成主线程的阻塞。  
+
+**原理：**  
+Fiber将渲染过程分为了几个优先级，每完成一个优先级的渲染进程就会把线程还给浏览器，直到浏览器空闲再允许React完成下一优先级的渲染进程。  
+
+**实现方式：**  
+在过去，也就是 React 16 的时代，Fiber使用了 window.requestIdleCallback(), 该方法的功能为：浏览器空闲时，执行回调。   
+
+而在现在，React 使用了 一个 scheduler package
+
